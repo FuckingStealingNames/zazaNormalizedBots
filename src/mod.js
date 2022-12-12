e6292a40753bf6a6d25bd27bec185066f9a73c6d
JustNU <ivancool108@gmail.com>
2022-12-12 04:58:06 +0300

    duplicated sanitar entries

--- src/mod.js
+++ src/mod.js
@@ -240,52 +240,6 @@ class Mod {
 			chances.equipment.TacticalVest = 100;
 		};
 		
-		// sanitar
-		if (botType === "bosssanitar") {
-
-			// headwear
-			inv.equipment.Headwear["5a7c4850e899ef00150be885"] = 1;
-			inv.equipment.Headwear["5aa7cfc0e5b5b00015693143"] = 1;
-			inv.equipment.Headwear["5aa7e276e5b5b000171d0647"] = 1;
-			inv.equipment.Headwear["5b40e1525acfc4771e1c6611"] = 1;
-			inv.equipment.Headwear["5b40e2bc5acfc40016388216"] = 1;
-			inv.equipment.Headwear["5aa7e454e5b5b0214e506fa2"] = 1;
-			inv.equipment.Headwear["5aa7e4a4e5b5b000137b76f2"] = 1;
-			inv.equipment.Headwear["5c091a4e0db834001d5addc8"] = 1;
-			inv.equipment.Headwear["5f60c74e3b85f6263c145586"] = 1;
-			
-			// armor vests
-			inv.equipment.ArmorVest["5fd4c474dd870108a754b241"] = 1;
-			
-			// rigs
-			inv.equipment.TacticalVest["5e4ac41886f77406a511c9a8"] = 1;
-			inv.equipment.TacticalVest["5c0e746986f7741453628fe5"] = 1;
-			inv.equipment.TacticalVest["5fd4c60f875c30179f5d04c2"] = 1;
-			inv.equipment.TacticalVest["603648ff5a45383c122086ac"] = 1;
-			inv.equipment.TacticalVest["6040dd4ddcf9592f401632d2"] = 1;
-			inv.equipment.TacticalVest["5ca20abf86f77418567a43f2"] = 1;
-			
-			// add visor to ZSh-1-2M
-			inv.mods["5aa7e454e5b5b0214e506fa2"] = {
-				"mod_equipment": [
-					"5aa7e3abe5b5b000171d064d"
-				]
-			};
-			
-			// add visor to ZSh-1-2M cover
-			inv.mods["5aa7e4a4e5b5b000137b76f2"] = {
-				"mod_equipment": [
-					"5aa7e3abe5b5b000171d064d"
-				]
-			};
-			
-			// adjust gear chances
-			chances.equipment.Headwear = 100;
-			chances.equipment.ArmorVest = 100;
-			chances.equipment.TacticalVest = 100;
-			chances.mods.mod_equipment = 70;
-		};
-		
 		// raiders
 		if (botType === "pmcbot") {
 
